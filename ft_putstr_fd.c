99cf0e20a8f4bf92e1e9d313dffdd8e3a7cb2dca
friky312 <165424672+friky312@users.noreply.github.com>
2024-09-24 15:21:26 +0200

    ft_putstr_fd.c
    Escribe una cadena en un descriptor de archivo dado

---
+++ ft_putstr_fd.c
@@ -0,0 +1,37 @@
+#include "libft.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	size_t	len;
+
+	len = ft_strlen(s);
+	write(fd, s, len);
+}
+
+//Otra forma de hacerlo
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (!s)
+		return ;
+	write(fd, s, ft_strlen(s));
+}
+
+//Una forma más corta todavía
+void	ft_putstr_fd(char *s, int fd)
+{
+	write(fd, s, ft_strlen(s));
+}
+
+//Este main sirve para las tres opciones
+int	main(void)
+{
+	char	s[2];
+	int		fd = 0;
+
+	if (read(fd, s, 1) > 0) 
+	{
+		s[1] = '\0';
+		ft_putstr_fd("Ff", 1);
+	}
+	return (0);
+}
